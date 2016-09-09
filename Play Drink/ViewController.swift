3a0cdc9fd805b7d8eb71602ec28105a92d7397bd
David <david.sobrinho33@yahoo.com.br>
2016-09-09 16:54:03 -0400

    Initial Commit

---
+++ Play Drink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Play Drink
+//
+//  Created by David Camurça on 9/9/16.
+//  Copyright © 2016 David Camurça. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
