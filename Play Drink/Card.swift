d31658bdc265fcd61b02e8f7a6bc72134467a3c0
David <david.sobrinho33@yahoo.com.br>
2016-09-13 17:00:53 -0400

    CardStore finish

--- Play Drink/Card.swift
+++ Play Drink/Card.swift
@@ -6,30 +6,18 @@
 //  Copyright © 2016 David Camurça. All rights reserved.
 //
 
-import Foundation
+
 import UIKit
 
 class Card {
     
-    var ID: Int?
-    var tittle: String?
-    var description: String?
-    var image:UIImage?
-    var background_front: UIImage?
-    var background_back: UIImage?
     
-    
-
-    init(id: Int, tittle: String, description: String,image: String, background_front: String,
-         background_back: String){
-        self.ID = id
-        self.tittle = tittle
-        self.description = description
-        self.image = UIImage(named: image)
-        self.background_front = UIImage(named: background_front)
-        self.background_back = UIImage(named:  background_back)
-        
-    }
+    var id: Int!
+    var tittle: String!
+    var description: String!
+    var image: UIImage!
+    var bg_front: UIImage!
+    var bg_back: UIImage!
     
     
 }
