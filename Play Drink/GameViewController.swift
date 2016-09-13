fbd634672372d53f7bef8c790ca1149b91352412
Anderson Oliveira <anderson_moliveira@icloud.com>
2016-09-13 16:09:17 -0400

    Criada a tela do jogo, e a navegação para ela

---
+++ Play Drink/GameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameViewController.swift
+//  Play Drink
+//
+//  Created by Anderson Oliveira on 13/09/16.
+//  Copyright © 2016 David Camurça. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
