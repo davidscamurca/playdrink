3e0b3de1197c79108f6aebb07f4c293ce8fe8794
Anderson Oliveira <anderson_moliveira@icloud.com>
2016-09-13 14:36:27 -0400

    Criada a navegação do menu até as telas de prejogo, cartas e regras

---
+++ Play Drink/MenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuViewController.swift
+//  Play Drink
+//
+//  Created by Anderson Oliveira on 12/09/16.
+//  Copyright © 2016 David Camurça. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
